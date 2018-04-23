536e60d3932b6ab152791bbeae7d8577d489d376
Vlad Barosan <vladdb@microsoft.com>
2018-04-23 13:04:07 -0700

    Add extra package

--- defaultStaticWebApp.go
+++ defaultStaticWebApp.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"fmt"
+	"github.com/deckarep/golang-set"
 	"net/http"
-	"os"
 )
 
 func main() {
-	port := fmt.Sprintf(":%s", os.Getenv("PORT"))
-    http.HandleFunc("/", f)
-    fmt.Println("Starting default static web app...")
+	port := fmt.Sprintf(":%s", "8033")
+	http.HandleFunc("/", f)
+	fmt.Println("Starting default static web app...")
 	http.ListenAndServe(port, nil)
 }
 
 func f(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello to go!")
 
+	basicColors := mapset.NewSet()
+	basicColors.Add("Red")
+	basicColors.Add("Blue")
+	basicColors.Add("Green")
+	if basicColors.Contains("Green") {
+		fmt.Fprintf(w, "Yay! 'Green' is a supported color")
+	} else {
+		fmt.Fprintf(w," What a disappointment! 'Green' is not a basic color")
+	}
 }
